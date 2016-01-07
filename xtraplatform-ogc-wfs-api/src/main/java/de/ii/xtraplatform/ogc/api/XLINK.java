1cc8e9dcea56549142deb942bd50fbcf06031b7d
azahnen <zahnen@interactive-instruments.de>
2016-01-07 13:28:00 +0100

    complete capabilities parser, closes #4

---
+++ xtraplatform-ogc-wfs-api/src/main/java/de/ii/xtraplatform/ogc/api/XLINK.java
@@ -0,0 +1,26 @@
+package de.ii.xtraplatform.ogc.api;
+
+/**
+ *
+ * @author zahnen
+ */
+public class XLINK extends VersionedVocabulary {
+
+    public enum VERSION {
+        DEFAULT;
+    }
+
+    public enum VOCABULARY {
+
+        NIL, NIL_TRUE;
+    }
+
+    static {
+        addWord(VERSION.DEFAULT, NAMESPACE.PREFIX, "xsi");
+        addWord(VERSION.DEFAULT, NAMESPACE.URI, "http://www.w3.org/2001/XMLSchema-instance");
+        
+        
+        addWord(VERSION.DEFAULT, VOCABULARY.NIL, "nil");
+        addWord(VERSION.DEFAULT, VOCABULARY.NIL_TRUE, "true");
+    }
+}
---
+++ xtraplatform-ogc-wfs-api/src/main/java/de/ii/xtraplatform/ogc/api/wfs/parser/LoggingWfsCapabilitiesAnalyzer.java
@@ -0,0 +1,7 @@
+package de.ii.xtraplatform.ogc.api.wfs.parser;
+
+/**
+ * @author zahnen
+ */
+public class LoggingWfsCapabilitiesAnalyzer {
+}
