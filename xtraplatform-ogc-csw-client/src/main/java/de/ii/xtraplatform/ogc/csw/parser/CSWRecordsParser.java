101bbfc5d97893169f62a2e91dce20525002804b
azahnen <zahnen@interactive-instruments.de>
2017-10-05 14:37:20 +0200

    change license to MPL 2.0

--- xtraplatform-ogc-csw-client/src/main/java/de/ii/xtraplatform/ogc/csw/parser/CSWRecordsParser.java
+++ xtraplatform-ogc-csw-client/src/main/java/de/ii/xtraplatform/ogc/csw/parser/CSWRecordsParser.java
@@ -1,17 +1,12 @@
 /**
- * Copyright 2016 interactive instruments GmbH
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2017 European Union, interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/**
+ * bla
  */
 package de.ii.xtraplatform.ogc.csw.parser;
 
--- xtraplatform-ogc-wfs-api/src/main/java/de/ii/xtraplatform/ogc/api/CSW.java
+++ xtraplatform-ogc-wfs-api/src/main/java/de/ii/xtraplatform/ogc/api/CSW.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2017 European Union, interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/**
+ * bla
+ */
 package de.ii.xtraplatform.ogc.api;
 
 import org.apache.http.client.utils.URIBuilder;
