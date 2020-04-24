07ed900b3829f3e0af3102074868db911102a4a8
ulugbek <akhmedjanov@interactive-instruments.de>
2020-04-24 12:44:14 +0200

    Implement migration to the new format

--- xtraplatform-features/src/main/java/de/ii/xtraplatform/features/app/FeatureProviderDataMigrationV1V2.java
+++ xtraplatform-features/src/main/java/de/ii/xtraplatform/features/app/FeatureProviderDataMigrationV1V2.java
@@ -5,13 +5,18 @@ import de.ii.xtraplatform.entity.api.EntityData;
 import de.ii.xtraplatform.event.store.EntityDataBuilder;
 import de.ii.xtraplatform.event.store.EntityMigration;
 import de.ii.xtraplatform.event.store.Identifier;
+import de.ii.xtraplatform.features.domain.FeatureProperty;
+import de.ii.xtraplatform.features.domain.FeaturePropertyV2;
 import de.ii.xtraplatform.features.domain.FeatureProviderDataV1;
 import de.ii.xtraplatform.features.domain.FeatureProviderDataV2;
+import de.ii.xtraplatform.features.domain.ImmutableFeaturePropertyV2;
 import de.ii.xtraplatform.features.domain.ImmutableFeatureProviderDataV1;
 import de.ii.xtraplatform.features.domain.ImmutableFeatureProviderDataV2;
 import de.ii.xtraplatform.features.domain.ImmutableFeatureTypeV2;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class FeatureProviderDataMigrationV1V2 implements EntityMigration<FeatureProviderDataV1, FeatureProviderDataV2> {
     @Override
@@ -40,17 +45,95 @@ public class FeatureProviderDataMigrationV1V2 implements EntityMigration<Feature
                 .featureProviderType(entityData.getFeatureProviderType())
                 .nativeCrs(entityData.getNativeCrs());
 
-        //TODO: transform flat properties structure from entityData into new nested structure
 
         entityData.getTypes().values().forEach(featureType -> {
 
+            String featureTypePath = "/" + featureType.getName();
             ImmutableFeatureTypeV2.Builder featureTypeNew = new ImmutableFeatureTypeV2.Builder()
-                    .path("");
+                    .path(featureTypePath);
 
+            String name = null;
+            String path = null;
+            Map<String, FeaturePropertyV2> newProperties = new LinkedHashMap<>();
+            for (Map.Entry<String, FeatureProperty> entry : featureType.getProperties().entrySet()) {
+
+                FeatureProperty fp = entry.getValue();
+                String[] nameTokens = entry.getKey().split("\\.");
+                FeaturePropertyV2.Role role = fp.getRole().isPresent() ? FeaturePropertyV2.Role.ID : null;
+                FeaturePropertyV2.Type type = FeaturePropertyV2.Type.valueOf(fp.getType().toString());
+
+                if (nameTokens.length == 1) {
+                    path = fp.getPath().replace(featureTypePath + "/", "");
+                    name = entry.getKey();
+                    if (nameTokens[0].endsWith("[]")) {
+                        featureTypeNew.putProperties(entry.getKey(),
+                                getFeaturePropertyV2ValueArray(entry.getKey(), path, type, role));
+                    } else {
+                        featureTypeNew.putProperties(entry.getKey(), getFeaturePropertyV2(name, path, type, role));
+                    }
+                } else {
+                    String parentName = nameTokens[0].replace("[]", "");
+                    FeaturePropertyV2.Type childType = FeaturePropertyV2.Type.valueOf(fp.getType().toString());
+
+                    if (!parentName.equals(name)) {
+                        name = parentName;
+                        path = fp.getPath().replace(featureTypePath + "/", "").split("(/[^/]+$)")[0];
+                        newProperties.clear();
+                    }
+
+                    if (nameTokens[0].contains("[]")) {
+                        type = FeaturePropertyV2.Type.OBJECT_ARRAY;
+                    } else {
+                        type = FeaturePropertyV2.Type.OBJECT;
+                    }
+
+                    if (nameTokens[1].endsWith("[]")) {
+                        String childName = nameTokens[1].replace("[]", "");
+                        String childPath = fp.getPath().replace(featureTypePath + "/" + path + "/", "");
+                        newProperties.put(childName,
+                                getFeaturePropertyV2ValueArray(childName, childPath, childType, role));
+                    } else {
+                        int lastSlashIndex = fp.getPath().lastIndexOf('/');
+                        newProperties.put(nameTokens[1],
+                                getFeaturePropertyV2(fp.getName(), fp.getPath().substring(lastSlashIndex + 1), childType, role));
+                    }
+                    featureTypeNew.putProperties(name, getFeaturePropertyV2(name, path, type, role, newProperties));
+                }
+            }
             builder.putTypes2(featureType.getName(), featureTypeNew);
+
         });
 
         return builder.build();
+    }
+
+    private FeaturePropertyV2 getFeaturePropertyV2(String name, String path, FeaturePropertyV2.Type type,
+                                                   FeaturePropertyV2.Role role) {
+        return new ImmutableFeaturePropertyV2.Builder()
+                .name(name)
+                .path(path)
+                .type(type)
+                .role(Optional.ofNullable(role))
+                .build();
+    }
+
+    private FeaturePropertyV2 getFeaturePropertyV2(String name, String path, FeaturePropertyV2.Type type,
+                                                   FeaturePropertyV2.Role role, Map<String, FeaturePropertyV2> properties) {
+        return new ImmutableFeaturePropertyV2.Builder()
+                .from(getFeaturePropertyV2(name, path, type, role))
+                .properties(properties)
+                .build();
+    }
+
+    private FeaturePropertyV2 getFeaturePropertyV2ValueArray(String name, String path, FeaturePropertyV2.Type valueType,
+                                                             FeaturePropertyV2.Role role) {
+        return new ImmutableFeaturePropertyV2.Builder()
+                .name(name)
+                .path(path)
+                .type(FeaturePropertyV2.Type.VALUE_ARRAY)
+                .role(Optional.ofNullable(role))
+                .valueType(valueType)
+                .build();
     }
 
     @Override
