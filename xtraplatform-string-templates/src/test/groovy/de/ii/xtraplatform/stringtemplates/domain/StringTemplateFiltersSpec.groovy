b021c9889d38dd1288c259f603bff3097e2bd669
ulugbek <akhmedjanov@interactive-instruments.de>
2020-12-09 13:21:55 +0100

    add string template filters unit tests

---
+++ xtraplatform-string-templates/src/test/groovy/de/ii/xtraplatform/stringtemplates/domain/StringTemplateFiltersSpec.groovy
@@ -0,0 +1,148 @@
+package de.ii.xtraplatform.stringtemplates.domain
+
+import spock.lang.Ignore
+import spock.lang.Specification
+
+class StringTemplateFiltersSpec extends Specification {
+
+    @Ignore
+    def 'All arguments are null'() {
+        when:
+            String result = StringTemplateFilters.applyTemplate(null, null, null, null)
+        then:
+            result.isEmpty()
+    }
+
+    @Ignore
+    def 'Template is null, value is present'() {
+        given:
+            String template = null
+            String value = "foobar"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result.isEmpty()
+    }
+
+    @Ignore
+    def 'Value is null, template is present'() {
+        given:
+            String template = "{{value | replace:'ABC':' '}}"
+            String value = null
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result.isEmpty()
+    }
+
+    def 'Template is empty, value is present'() {
+        given:
+            String template = ""
+            String value = "foobar"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result.isEmpty()
+    }
+
+    def 'Value is empty, template is present'() {
+        given:
+            String template = "{{value | replace:'ABC':' '}}"
+            String value = ""
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result.isEmpty()
+    }
+
+    def 'replace test'() {
+        given:
+            String template = "{{value | replace:'ABC':' '}}"
+            String value = "foobarABCtest"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "foobar test"
+    }
+
+    def 'append test'() {
+        given:
+            String template = "{{value | append:'bar'}}"
+            String value = "foo"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "foobar"
+    }
+
+    def 'prepend test'() {
+        given:
+            String template = "{{value | prepend:'bar'}}"
+            String value = "foo"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "barfoo"
+    }
+
+    def 'toLower test'() {
+        given:
+            String template = "{{value | toLower}}"
+            String value = "fooBAR"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "foobar"
+    }
+
+    def 'toUpper test'() {
+        given:
+            String template = "{{value | toUpper}}"
+            String value = "fooBAR"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "FOOBAR"
+    }
+
+    def 'urlEncode test'() {
+        given:
+            String template = "{{value | urlEncode}}"
+            String value = "query=foo&bar test"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "query%3Dfoo%26bar+test"
+    }
+
+    def 'urlencode test'() {
+        given:
+        String template = "{{value | urlencode}}"
+        String value = "query=foo&bar test"
+        when:
+        String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+        result == "query%3Dfoo%26bar+test"
+    }
+
+    def 'markdown test'() {
+        given:
+            String template = "{{value | markdown}}"
+            String value = "foo*bar*"
+        when:
+            String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+            result == "foo<em>bar</em>"
+    }
+
+    def 'Multiple filters'() {
+        given:
+        String template = "{{value | prepend:'query=' | append:' test' | urlencode}}"
+        String value = "foo&bar"
+        when:
+        String result = StringTemplateFilters.applyTemplate(template, value)
+        then:
+        result == "query%3Dfoo%26bar+test"
+    }
+
+}
