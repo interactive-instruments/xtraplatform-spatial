e07909ca095050a718f74231fe9c9c646fb9d511
Andreas Zahnen <zahnen@interactive-instruments.de>
2021-02-24 13:17:12 +0100

    move validation result to core (#45)

--- xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/TypeInfoValidator.java
+++ xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/TypeInfoValidator.java
@@ -7,49 +7,18 @@
  */
 package de.ii.xtraplatform.features.domain;
 
-import de.ii.xtraplatform.features.domain.FeatureProviderDataV2.VALIDATION;
-import java.util.List;
-import org.immutables.value.Value;
+import de.ii.xtraplatform.store.domain.entities.ValidationResult.MODE;
+import de.ii.xtraplatform.store.domain.entities.ValidationResult;
 
 public interface TypeInfoValidator {
 
-  default ValidationResult validate(FeatureStoreTypeInfo typeInfo, VALIDATION mode) {
+  default ValidationResult validate(FeatureStoreTypeInfo typeInfo, MODE mode) {
     return typeInfo.getInstanceContainers().get(0).getAllAttributesContainers().stream()
         .map(attributesContainer -> validate(typeInfo.getName(), attributesContainer, mode))
         .reduce(ValidationResult.of(), ValidationResult::mergeWith);
   }
 
   ValidationResult validate(
-      String typeName, FeatureStoreAttributesContainer attributesContainer, VALIDATION mode);
+      String typeName, FeatureStoreAttributesContainer attributesContainer, MODE mode);
 
-  @Value.Immutable
-  interface ValidationResult {
-
-    static ValidationResult of() {
-      return ImmutableValidationResult.builder().mode(VALIDATION.NONE).build();
-    }
-
-    default ValidationResult mergeWith(ValidationResult other) {
-      return ImmutableValidationResult.builder()
-          .from(this)
-          .mode(other.getMode())
-          .addAllErrors(other.getErrors())
-          .addAllStrictErrors(other.getStrictErrors())
-          .addAllWarnings(other.getWarnings())
-          .build();
-    }
-
-    VALIDATION getMode();
-
-    List<String> getErrors();
-
-    List<String> getStrictErrors();
-
-    List<String> getWarnings();
-
-    @Value.Derived
-    default boolean isSuccess() {
-      return getErrors().isEmpty() && (getMode() == VALIDATION.LAX || getStrictErrors().isEmpty());
-    }
-  }
 }
