2de621a5c3f4dd7acc31bd4e98212000b7c3831b
Clemens Portele <portele@interactive-instruments.de>
2021-09-17 18:22:04 +0200

    avoid NPE, if the meta result is empty

--- xtraplatform-features-sql/src/main/java/de/ii/xtraplatform/feature/provider/sql/app/FeatureQueryTransformerSql.java
+++ xtraplatform-features-sql/src/main/java/de/ii/xtraplatform/feature/provider/sql/app/FeatureQueryTransformerSql.java
@@ -81,7 +81,7 @@ class FeatureQueryTransformerSql implements FeatureQueryTransformer<SqlQueries, 
         .getValueQueryTemplates()
         .stream()
     .map(valueQueryTemplate -> valueQueryTemplate.generateValueQuery(featureQuery.getLimit(), featureQuery.getOffset(), sortKeys,
-        featureQuery.getFilter(), Optional.of(Tuple.of(metaResult.getMinKey(), metaResult.getMaxKey()))));
+        featureQuery.getFilter(), (Objects.nonNull(metaResult.getMinKey()) && Objects.nonNull(metaResult.getMaxKey())) ? Optional.of(Tuple.of(metaResult.getMinKey(), metaResult.getMaxKey())) : Optional.empty()));
 
     return new ImmutableSqlQueries.Builder()
         .metaQuery(metaQuery)
