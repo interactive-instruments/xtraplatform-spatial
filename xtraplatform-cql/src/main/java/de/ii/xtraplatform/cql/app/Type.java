b1d15f16148f53464cad0a4a7ec906da17db5be2
Clemens Portele <portele@interactive-instruments.de>
2022-02-20 17:50:51 +0100

    Add type checks for CQL2 expressions (#109)
    * add visitor for type checks
    
    * avoid duplicate entries
    
    * add more information
    
    * update unit tests
    
    * update visitor to parse type tree for all nodes
    
    * cover additional cases
    
    * improve error message

---
+++ xtraplatform-cql/src/main/java/de/ii/xtraplatform/cql/app/Type.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2022 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.xtraplatform.cql.app;
+
+public enum Type {
+    String, Boolean, Integer, Long, Double,
+    LocalDate, Instant, Interval, OPEN,
+    Geometry,
+    List,
+    UNKNOWN;
+
+    public String schemaType() {
+        switch (this) {
+            case String:
+                return "STRING";
+            case Boolean:
+                return "BOOLEAN";
+            case Integer:
+            case Long:
+                return "INTEGER";
+            case Double:
+                return "FLOAT";
+            case LocalDate:
+                return "DATE";
+            case OPEN:
+            case Instant:
+                return "DATETIME";
+            case Interval:
+                return "STRING";
+            case Geometry:
+                return "GEOMETRY";
+            case List:
+                return "ARRAY";
+            default:
+                return "unknown";
+        }
+    }
+}
