798644ad8eb6a5fcf01250d290b94828844c0558
azahnen <zahnen@interactive-instruments.de>
2022-07-12 08:01:06 +0200

    xtraplatform changes

--- xtraplatform-feature-changes-sql/src/main/java/de/ii/xtraplatform/feature/changes/sql/app/JacksonSubTypeIdsFeatureChanges.java
+++ xtraplatform-feature-changes-sql/src/main/java/de/ii/xtraplatform/feature/changes/sql/app/JacksonSubTypeIdsFeatureChanges.java
@@ -8,11 +8,12 @@
 package de.ii.xtraplatform.feature.changes.sql.app;
 
 import com.github.azahnen.dagger.annotations.AutoBind;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableList;
+import de.ii.xtraplatform.base.domain.ImmutableJacksonSubType;
 import de.ii.xtraplatform.base.domain.JacksonSubTypeIds;
 import de.ii.xtraplatform.feature.changes.sql.domain.FeatureChangesPgConfiguration;
 import de.ii.xtraplatform.features.domain.ExtensionConfiguration;
-import java.util.Map;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -24,11 +25,12 @@ public class JacksonSubTypeIdsFeatureChanges implements JacksonSubTypeIds {
   public JacksonSubTypeIdsFeatureChanges() {}
 
   @Override
-  public Map<Class<?>, String> getMapping() {
-    return new ImmutableMap.Builder<Class<?>, String>()
-        .put(
-            FeatureChangesPgConfiguration.class,
-            ExtensionConfiguration.getIdentifier(FeatureChangesPgConfiguration.class))
-        .build();
+  public List<JacksonSubType> getSubTypes() {
+    return ImmutableList.of(
+        ImmutableJacksonSubType.builder()
+            .superType(ExtensionConfiguration.class)
+            .subType(FeatureChangesPgConfiguration.class)
+            .id(ExtensionConfiguration.getIdentifier(FeatureChangesPgConfiguration.class))
+            .build());
   }
 }
