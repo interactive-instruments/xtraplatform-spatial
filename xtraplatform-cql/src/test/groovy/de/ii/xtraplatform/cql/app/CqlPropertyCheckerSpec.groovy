cf06eddd62c5e63156f7fdfaa7ef12b2ffa6726c
Ege Cabuk <94187749+CabukEge@users.noreply.github.com>
2022-09-20 09:16:31 +0200

    Added tests (#129)
    * Added tests
    
    * Added Tests
    
    * Added Test crsTransformerFilterToCrs84 is present
    
    * Added 3th visitor
    
    * Added tests for CqlTypeCheckerSpec.groovy
    
    * Added tests
    
    * Correction for visit with Envelope.
    
    * Added tests
    
    * Added tests
    
    * Added tests covering mapTemporalOperators
    
    * Added tests covering missed parts of the code
    
    * Added markdown test with link
    
    * exclude generated antlr classes from coverage
    
    * add markdown newline test
    
    * Added tests
    
    * Added tests
    
    Co-authored-by: Andreas Zahnen <zahnen@interactive-instruments.de>

--- xtraplatform-cql/src/test/groovy/de/ii/xtraplatform/cql/app/CqlPropertyCheckerSpec.groovy
+++ xtraplatform-cql/src/test/groovy/de/ii/xtraplatform/cql/app/CqlPropertyCheckerSpec.groovy
@@ -8,6 +8,12 @@
 package de.ii.xtraplatform.cql.app
 
 import de.ii.xtraplatform.cql.domain.Cql
+import de.ii.xtraplatform.cql.domain.Cql2Expression
+import de.ii.xtraplatform.cql.domain.Geometry
+import de.ii.xtraplatform.cql.domain.Property
+import de.ii.xtraplatform.cql.domain.SIntersects
+import de.ii.xtraplatform.cql.domain.SpatialLiteral
+import de.ii.xtraplatform.crs.domain.OgcCrs
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -42,5 +48,144 @@ class CqlPropertyCheckerSpec extends Specification {
         invalidProperties2.size() == 1
         invalidProperties2.get(0) == "owner"
     }
+
+    def 'Test covering visit(BinaryArrayOperation, List) and visit(ArrayLiteral, List)'() {
+        given:
+
+        def allowedProperties = ["doors", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_44.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 0
+
+    }
+
+    def 'Test covering visit(Interval, List) and visit(BinaryTemporalOperation, List)'() {
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_TINTERSECTS.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 2
+        invalidProperties.get(0) == "startDate"
+        invalidProperties.get(1) == "endDate"
+
+    }
+
+    def 'Test covering visit(SpatialLiteral, List)'() {
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_17.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 2
+        invalidProperties.get(0) == "floors"
+        invalidProperties.get(1) == "geometry"
+
+    }
+
+    def 'Test covering visit(Like, List)'() {
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_3.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 1
+        invalidProperties.get(0) == "owner"
+
+    }
+
+    def 'Test covering visit(Between, List)'() {
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_NESTED_FUNCTION.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 1
+        invalidProperties.get(0) == "filterValues.measure"
+
+    }
+
+    def 'Test covering visit(In, List)'() {
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_LOWER.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 1
+        invalidProperties.get(0) == "road_class"
+
+    }
+
+    def 'Test covering visit(Casei, List)'() {
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_CASEI.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 1
+        invalidProperties.get(0) == "road_class"
+
+    }
+
+    def 'Test covering visit(Accenti, List)'() {
+
+        given:
+
+        def allowedProperties = ["event_date", "theme.concept"]
+        CqlPropertyChecker visitor = new CqlPropertyChecker(allowedProperties)
+
+        when:
+
+        def invalidProperties = CqlFilterExamples.EXAMPLE_ACCENTI.accept(visitor, true)
+
+        then:
+
+        invalidProperties.size() == 1
+        invalidProperties.get(0) == "road_class"
+
+    }
+
 
 }
