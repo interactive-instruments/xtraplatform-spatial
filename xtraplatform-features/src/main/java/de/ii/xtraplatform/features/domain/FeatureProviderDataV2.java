387657ec5e251eb6e3f136a297458e255551553c
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-03-21 17:32:02 +0100

    cleanup

--- xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/FeatureProviderDataV2.java
+++ xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/FeatureProviderDataV2.java
@@ -182,13 +182,6 @@ public interface FeatureProviderDataV2 extends ProviderData, AutoEntity, Extenda
   @JsonMerge
   BuildableMap<FeatureSchema, ImmutableFeatureSchema.Builder> getTypes();
 
-  /**
-   * @langEn Definition of external feature types, see [below](#types).
-   * @langDe Definition of external object types, see [below](#types).
-   */
-  @JsonMerge
-  BuildableMap<FeatureSchemaExt, ImmutableFeatureSchemaExt.Builder> getExternalTypes();
-
   @DocIgnore
   Map<String, Map<String, String>> getCodelists();
 
@@ -247,23 +240,6 @@ public interface FeatureProviderDataV2 extends ProviderData, AutoEntity, Extenda
     @JsonProperty(value = "types")
     public T putTypes2(String key, ImmutableFeatureSchema.Builder builder) {
       return putTypes(key, builder.name(key));
-    }
-
-    @JsonIgnore
-    public abstract Map<String, ImmutableFeatureSchemaExt.Builder> getExternalTypes();
-
-    @JsonProperty(value = "externalTypes")
-    public Map<String, ImmutableFeatureSchemaExt.Builder> getExternalTypes2() {
-      Map<String, ImmutableFeatureSchemaExt.Builder> types = getExternalTypes();
-
-      return new ApplyKeyToValueMap<>(types, (key, builder) -> builder.name(key));
-    }
-
-    public abstract T putExternalTypes(String key, ImmutableFeatureSchemaExt.Builder builder);
-
-    @JsonProperty(value = "externalTypes")
-    public T putExternalTypes2(String key, ImmutableFeatureSchemaExt.Builder builder) {
-      return putExternalTypes(key, builder.name(key));
     }
 
     public abstract T id(String id);
