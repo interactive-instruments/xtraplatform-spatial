0f74461f0d9bba2a7db98eee72c07a114f63c3a0
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-07-09 14:41:36 +0200

    refactoring, fix concat value type

--- xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/FeatureTokenTransformerSchemaMappings.java
+++ xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/FeatureTokenTransformerSchemaMappings.java
@@ -15,6 +15,7 @@ import de.ii.xtraplatform.features.domain.transform.FeaturePropertySchemaTransfo
 import de.ii.xtraplatform.features.domain.transform.FeaturePropertyTransformerFlatten;
 import de.ii.xtraplatform.features.domain.transform.FeaturePropertyTransformerFlatten.INCLUDE;
 import de.ii.xtraplatform.features.domain.transform.FeaturePropertyTransformerObjectReduce;
+import de.ii.xtraplatform.features.domain.transform.MappingOperationsTransformer;
 import de.ii.xtraplatform.features.domain.transform.PropertyTransformations;
 import de.ii.xtraplatform.features.domain.transform.TransformerChain;
 import java.util.AbstractMap.SimpleImmutableEntry;
@@ -55,6 +56,7 @@ public class FeatureTokenTransformerSchemaMappings extends FeatureTokenTransform
   private final List<List<String>> indexedArrays;
   private final List<List<String>> openedArrays;
   private final Set<String> coalesces;
+  private final MappingOperationsTransformer mappingOperationsTransformer;
 
   public FeatureTokenTransformerSchemaMappings(
       Map<String, PropertyTransformations> propertyTransformations) {
@@ -65,6 +67,7 @@ public class FeatureTokenTransformerSchemaMappings extends FeatureTokenTransform
     this.contextTransformerChains = new LinkedHashMap<>();
     this.nestingTrackers = new LinkedHashMap<>();
     this.coalesces = new HashSet<>();
+    this.mappingOperationsTransformer = new MappingOperationsTransformer();
   }
 
   @Override
@@ -179,6 +182,7 @@ public class FeatureTokenTransformerSchemaMappings extends FeatureTokenTransform
     this.schemaTransformerChain = schemaTransformerChains.get(context.type());
     this.contextTransformerChain = contextTransformerChains.get(context.type());
     this.coalesces.clear();
+    this.mappingOperationsTransformer.reset();
 
     if (flattened.get(context.type())) {
       newContext.putTransformed(FeaturePropertyTransformerFlatten.TYPE, "TRUE");
@@ -190,22 +194,13 @@ public class FeatureTokenTransformerSchemaMappings extends FeatureTokenTransform
 
     newContext.setType(context.type());
 
-    // TODO: why is this needed at all, what does withTargetPaths do?
-    // if (context.query() instanceof MultiFeatureQuery) {
-    // TODO: slow, precompute, same for original in decoder
-    // SchemaMapping schemaMapping = SchemaMapping.withTargetPaths(context.mapping());
-    // newContext.setMapping(schemaMapping);
-    // }
-
     getDownstream().onFeatureStart(newContext);
   }
 
   @Override
   public void onFeatureEnd(ModifiableContext<FeatureSchema, SchemaMapping> context) {
     while (nestingTracker.isNested()) {
-      // nestingTracker.close();
       if (nestingTracker.inObject()) {
-        // nestingTracker.closeObject();
         closeObject();
       } else if (nestingTracker.inArray()) {
         closeArray();
@@ -332,97 +327,11 @@ public class FeatureTokenTransformerSchemaMappings extends FeatureTokenTransform
       pushValue();
       return;
     }
-    if (context.schema().isEmpty()) {
-      if (LOGGER.isTraceEnabled()) {
-        LOGGER.trace(
-            "VALUE NOT FOUND {} {}",
-            context.pathAsString(),
-            Objects.nonNull(context.mapping())
-                ? context.mapping().getTargetSchemasByPath().keySet()
-                : "{}");
-      }
-      return;
-    }
 
     FeatureSchema schema = context.schema().get();
 
-    // TODO: when to clear valueBuffer
-    // TODO: what about parent arrays
-    // TODO: special value buffer for choice
-    if (schema.getEffectiveSourcePaths().size() > 1) {
-      String column = context.path().get(context.path().size() - 1);
-      String column2 = column;
-      for (int i = 0; i < schema.getEffectiveSourcePaths().size(); i++) {
-        if (String.join("/", context.path()).endsWith(schema.getEffectiveSourcePaths().get(i))) {
-          column2 = schema.getEffectiveSourcePaths().get(i);
-        }
-      }
-      int index = schema.getEffectiveSourcePaths().indexOf(column2);
-      if (schema.isArray()) {
-        if (index >= 0) {
-          List<Integer> indexes =
-              new ArrayList<>(
-                  index == 0
-                      ? context.indexes()
-                      : context.indexes().subList(0, Math.max(0, context.indexes().size() - 1)));
-          indexes.add(index + 1);
-          context.setIndexes(indexes);
-
-          if (schema.getConcat().size() > index) {
-            context.setValueType(schema.getConcat().get(index).getType());
-          }
-        }
-      } else if (!schema.getCoalesce().isEmpty()) {
-        if (Objects.nonNull(context.value()) && !coalesces.contains(schema.getFullPathAsString())) {
-          coalesces.add(schema.getFullPathAsString());
-          if (schema.getCoalesce().size() > index) {
-            context.setValueType(schema.getCoalesce().get(index).getType());
-          }
-        } else {
-          return;
-        }
-      }
-    }
-
-    if (schema.getAdditionalInfo().containsKey("concatIndex")) {
-      int index = Integer.parseInt(schema.getAdditionalInfo().get("concatIndex"));
-      int arrayIndex =
-          schema.getAdditionalInfo().containsKey("concatArray")
-              ? Math.max(0, (int) context.index() - 1)
-              : 0;
-
-      if (index >= 1) {
-        int prev = Integer.parseInt(context.transformed().get("concatIndex"));
-        int prevArray = Integer.parseInt(context.transformed().get("concatArray"));
-        if (prev < index || prevArray < arrayIndex) {
-          List<Integer> indexes =
-              new ArrayList<>(
-                  newContext.indexes().subList(0, Math.max(0, newContext.indexes().size() - 1)));
-          indexes.add(newContext.indexes().get(newContext.indexes().size() - 1) + 1);
-
-          closeObject();
-          newContext.setIndexes(indexes);
-          FeatureSchema parentSchema = context.parentSchemas().get(0);
-          openObject(parentSchema);
-        }
-      }
-      context.putTransformed("concatIndex", String.valueOf(index));
-      context.putTransformed("concatArray", String.valueOf(arrayIndex));
-    } else if (schema.getAdditionalInfo().containsKey("coalesceIndex")) {
-      int arrayIndex =
-          schema.getAdditionalInfo().containsKey("coalesceArray")
-              ? Math.max(0, (int) context.index() - 1)
-              : 0;
-      if (Objects.nonNull(context.value())
-          && !coalesces.contains(schema.getParentPath().toString())) {
-        coalesces.add(schema.getParentPath().toString());
-        context.putTransformed(
-            "coalesceIndex", schema.getAdditionalInfo().get("coalesceIndex") + arrayIndex);
-      } else if (!Objects.equals(
-          schema.getAdditionalInfo().get("coalesceIndex") + arrayIndex,
-          context.transformed().get("coalesceIndex"))) {
-        return;
-      }
+    if (!mappingOperationsTransformer.transform(context, newContext)) {
+      return;
     }
 
     handleNesting(schema, context.parentSchemas(), context.indexes());
@@ -482,6 +391,10 @@ public class FeatureTokenTransformerSchemaMappings extends FeatureTokenTransform
       closeObject();
       newContext.setIndexes(indexes);
       openObject(schema);
+    } else if (newContext.transformed().containsKey("concatNewObject")) {
+      newContext.transformed().remove("concatNewObject");
+      closeObject();
+      openObject(parentSchemas.get(0));
     }
 
     if (schema.isArray() && !nestingTracker.isSamePath(schema.getFullPath())) {
---
+++ xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/transform/MappingOperationsTransformer.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2022 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.xtraplatform.features.domain.transform;
+
+import de.ii.xtraplatform.features.domain.FeatureEventHandler.ModifiableContext;
+import de.ii.xtraplatform.features.domain.FeatureSchema;
+import de.ii.xtraplatform.features.domain.SchemaMapping;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MappingOperationsTransformer implements ContextTransformer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MappingOperationsTransformer.class);
+
+  private final Set<String> coalesces;
+  private String concatPath;
+  private int concatIndex;
+  private int concatArrayIndex;
+  private int concatItem;
+  private String coalescePath;
+  private String coalesceIndex;
+
+  public MappingOperationsTransformer() {
+    this.coalesces = new HashSet<>();
+  }
+
+  @Override
+  public void reset() {
+    this.concatPath = null;
+    this.coalescePath = null;
+    coalesces.clear();
+  }
+
+  @Override
+  public boolean transform(
+      ModifiableContext<FeatureSchema, SchemaMapping> context,
+      ModifiableContext<FeatureSchema, SchemaMapping> newContext) {
+    if (context.schema().isEmpty()) {
+      if (LOGGER.isTraceEnabled()) {
+        LOGGER.trace(
+            "PATH NOT FOUND {} {}",
+            context.pathAsString(),
+            Objects.nonNull(context.mapping())
+                ? context.mapping().getTargetSchemasByPath().keySet()
+                : "{}");
+      }
+      return false;
+    }
+
+    FeatureSchema schema = context.schema().get();
+
+    if (schema.getEffectiveSourcePaths().size() > 1) {
+      int index = getIndex(context.path(), schema.getEffectiveSourcePaths());
+
+      if (!schema.getConcat().isEmpty()) {
+        return valueConcat(schema, index, context);
+      } else if (!schema.getCoalesce().isEmpty()) {
+        return valueCoalesce(schema, index, context);
+      }
+    }
+
+    if (schema.getAdditionalInfo().containsKey("concatIndex")) {
+      return objectConcat(schema, context, newContext);
+    } else if (schema.getAdditionalInfo().containsKey("coalesceIndex")) {
+      return objectCoalesce(schema, context);
+    }
+
+    return true;
+  }
+
+  private int getIndex(List<String> path, List<String> sourcePaths) {
+    String column = path.get(path.size() - 1);
+    for (String sourcePath : sourcePaths) {
+      if (String.join("/", path).endsWith(sourcePath)) {
+        column = sourcePath;
+      }
+    }
+    return sourcePaths.indexOf(column);
+  }
+
+  private boolean valueConcat(
+      FeatureSchema schema, int index, ModifiableContext<FeatureSchema, SchemaMapping> context) {
+    if (Objects.nonNull(context.value())) {
+      if (!Objects.equals(concatPath, schema.getFullPathAsString())) {
+        this.concatPath = schema.getFullPathAsString();
+        this.concatItem = 0;
+      }
+
+      List<Integer> indexes =
+          new ArrayList<>(
+              schema.getConcat().get(index).isArray()
+                  ? context.indexes().subList(0, Math.max(0, context.indexes().size() - 1))
+                  : context.indexes());
+      indexes.add(++concatItem);
+
+      context.setIndexes(indexes);
+    }
+    if (schema.getConcat().size() > index) {
+      context.setValueType(
+          schema
+              .getConcat()
+              .get(index)
+              .getValueType()
+              .orElse(schema.getConcat().get(index).getType()));
+    }
+
+    return true;
+  }
+
+  private boolean valueCoalesce(
+      FeatureSchema schema, int index, ModifiableContext<FeatureSchema, SchemaMapping> context) {
+    if (Objects.nonNull(context.value()) && !coalesces.contains(schema.getFullPathAsString())) {
+      this.coalesces.add(schema.getFullPathAsString());
+
+      if (schema.getCoalesce().size() > index) {
+        context.setValueType(schema.getCoalesce().get(index).getType());
+      }
+    } else {
+      return false;
+    }
+
+    return true;
+  }
+
+  private boolean objectConcat(
+      FeatureSchema schema,
+      ModifiableContext<FeatureSchema, SchemaMapping> context,
+      ModifiableContext<FeatureSchema, SchemaMapping> newContext) {
+    int index = Integer.parseInt(schema.getAdditionalInfo().get("concatIndex"));
+    int arrayIndex =
+        schema.getAdditionalInfo().containsKey("concatArray")
+            ? Math.max(0, (int) context.index() - 1)
+            : 0;
+
+    if (!Objects.equals(concatPath, context.parentSchemas().get(0).getFullPathAsString())) {
+      this.concatPath = context.parentSchemas().get(0).getFullPathAsString();
+      this.concatItem = 0;
+      this.concatIndex = -1;
+      this.concatArrayIndex = -1;
+    }
+
+    if (concatIndex < index || concatArrayIndex < arrayIndex) {
+      /*List<Integer> indexes =
+          new ArrayList<>(
+              newContext.indexes().subList(0, Math.max(0, newContext.indexes().size() - 1)));
+      indexes.add(newContext.indexes().get(newContext.indexes().size() - 1) + 1);
+
+      closeObject();
+      newContext.setIndexes(indexes);
+      FeatureSchema parentSchema = context.parentSchemas().get(0);
+      openObject(parentSchema);*/
+      /*List<Integer> indexes =
+          new ArrayList<>(
+              concatItem > 0
+                  ? context.indexes().subList(0, Math.max(0, context.indexes().size() - 1))
+                  : context.indexes());
+      indexes.add(++concatItem);
+
+      LOGGER.debug("INDEXES {} {}", context.indexes(), indexes);
+      context.setIndexes(indexes);*/
+      if (++concatItem > 1) {
+        // newContext.setIndexes(indexes);
+        List<Integer> indexes =
+            new ArrayList<>(
+                newContext.indexes().subList(0, Math.max(0, newContext.indexes().size() - 1)));
+        indexes.add(newContext.indexes().get(newContext.indexes().size() - 1) + 1);
+
+        newContext.setIndexes(indexes);
+        newContext.putTransformed("concatNewObject", "true");
+      }
+    }
+
+    this.concatIndex = index;
+    this.concatArrayIndex = arrayIndex;
+
+    return true;
+  }
+
+  private boolean objectCoalesce(
+      FeatureSchema schema, ModifiableContext<FeatureSchema, SchemaMapping> context) {
+    int arrayIndex =
+        schema.getAdditionalInfo().containsKey("coalesceArray")
+            ? Math.max(0, (int) context.index() - 1)
+            : 0;
+    if (Objects.nonNull(context.value())
+        && !Objects.equals(coalescePath, schema.getFullPathAsString())) {
+      this.coalescePath = schema.getParentPath().toString();
+      this.coalesceIndex = schema.getAdditionalInfo().get("coalesceIndex") + arrayIndex;
+    } else if (!Objects.equals(
+        schema.getAdditionalInfo().get("coalesceIndex") + arrayIndex, coalesceIndex)) {
+      return false;
+    }
+
+    return true;
+  }
+}
