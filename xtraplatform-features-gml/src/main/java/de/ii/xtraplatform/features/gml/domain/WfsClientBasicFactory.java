d9aa7e39c7f9f768191635cb8548f7b61caea63b
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-11-08 10:27:15 +0100

    refactor wfs auto  mode

---
+++ xtraplatform-features-gml/src/main/java/de/ii/xtraplatform/features/gml/domain/WfsClientBasicFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2023 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.xtraplatform.features.gml.domain;
+
+public interface WfsClientBasicFactory {
+
+  WfsClientBasic create(
+      String providerType, String providerId, ConnectionInfoWfsHttp connectionInfo);
+
+  void dispose(WfsClientBasic wfsClient);
+}
