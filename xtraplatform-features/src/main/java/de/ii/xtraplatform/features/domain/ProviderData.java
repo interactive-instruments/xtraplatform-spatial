7459df29e77c0d2f2427b0676c83a1da0acf89b3
Clemens Portele <portele@interactive-instruments.de>
2024-01-09 14:41:07 +0530

    support removing change listeners (#251)

--- xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/ProviderData.java
+++ xtraplatform-features/src/main/java/de/ii/xtraplatform/features/domain/ProviderData.java
@@ -8,6 +8,7 @@
 package de.ii.xtraplatform.features.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.ii.xtraplatform.docs.DocIgnore;
 import de.ii.xtraplatform.entities.domain.EntityData;
 import java.util.Optional;
 import org.immutables.value.Value;
@@ -28,5 +29,23 @@ public interface ProviderData extends EntityData {
   default Optional<String> getEntitySubType() {
     return Optional.of(
         String.format("%s/%s", getProviderType(), getProviderSubType()).toLowerCase());
+  }
+
+  // We need to add the @Value.Auxiliary annotation here again, otherwise createdAt and lastModified
+  // are still included in the hash. This may be a bug in the immutables?
+  @DocIgnore
+  @Value.Default
+  @Value.Auxiliary
+  @Override
+  default long getCreatedAt() {
+    return EntityData.super.getCreatedAt();
+  }
+
+  @DocIgnore
+  @Value.Default
+  @Value.Auxiliary
+  @Override
+  default long getLastModified() {
+    return EntityData.super.getLastModified();
   }
 }
